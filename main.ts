8e10d0668b6f34e14c92dde31b1dbe3481e687cd
Thomas Jensen <thomas.jensen_@outlook.com>
2026-02-07 20:13:50 +0100

    Even more CSS optimisations

--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
 import { App, staticFiles, trailingSlashes } from 'fresh'
+import { options } from 'preact'
 import type { ExtendedState } from './utils.ts'
 import { securityHeaders } from '~/lib/security.ts'
+
+const originalVNodeHook = options.vnode
+options.vnode = (vnode) => {
+	if (typeof vnode.type === 'string' && vnode.type === 'link') {
+		const props = vnode.props as Record<string, unknown>
+		const href = typeof props.href === 'string' ? props.href : ''
+		if (
+			props.rel === 'stylesheet' &&
+			href.startsWith('/assets/') &&
+			href.includes('.css')
+		) {
+			props.media = 'print'
+			props.onload = "this.media='all'"
+		}
+	}
+
+	originalVNodeHook?.(vnode)
+}
 
 export const app = new App<ExtendedState>()
 	// Expose the request origin for resource-hint links in the app shell.
