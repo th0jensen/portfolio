0dbe017850140c13b94ef5e640d8b822aecfbb8a
Thomas Jensen <thomas.jensen_@outlook.com>
2026-03-20 19:40:05 +0100

    fixed some mobile viewport issues

--- components/hero/HeroMobileImage.tsx
+++ components/hero/HeroMobileImage.tsx
@@ -8,7 +8,7 @@ export default function HeroMobileImage(
 ) {
 	return (
 		<>
-			<div className='absolute top-0 left-0 right-0 md:hidden h-[62vh] overflow-visible z-0'>
+			<div className='absolute top-0 left-0 right-0 z-0 h-[58vh] overflow-visible md:hidden'>
 				<img
 					src={headshotUrl}
 					alt={alt}
@@ -18,7 +18,7 @@ export default function HeroMobileImage(
 					className='h-full w-full object-cover object-[50%_35%]'
 				/>
 			</div>
-			<div className='pointer-events-none absolute top-[120px] left-0 right-0 h-[calc(62vh-100px)] bg-[linear-gradient(to_bottom,transparent_0,transparent_calc(35%+50px),hsl(var(--background))_75%,hsl(var(--background))_100%)] dark:bg-[linear-gradient(to_bottom,transparent_0,transparent_calc(35%+50px),hsl(var(--background))_75%,hsl(var(--background))_100%)] md:hidden z-10' />
+			<div className='pointer-events-none absolute left-0 right-0 top-16 z-10 h-[calc(58vh-64px)] bg-[linear-gradient(to_bottom,rgba(255,255,255,0.16)_0%,rgba(255,255,255,0.08)_22%,transparent_38%,hsl(var(--background))_78%,hsl(var(--background))_100%)] dark:bg-[linear-gradient(to_bottom,rgba(0,0,0,0.4)_0%,rgba(0,0,0,0.18)_22%,transparent_38%,hsl(var(--background))_78%,hsl(var(--background))_100%)] md:hidden' />
 		</>
 	)
 }
