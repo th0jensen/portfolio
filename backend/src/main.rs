9e79ec68e3a21f74037df8f243f56d001000cfd6
Thomas Jensen <thomas.jensen_@outlook.com>
2026-04-23 23:14:19 +0200

    added json to logs to improve victorialogs parsing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -37,6 +37,8 @@ async fn main() {
     let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
 
     tracing_subscriber::fmt()
+        .json()
+        .with_level(true)
         .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(
             |_| EnvFilter::new("backend=info,tower_http=warn,axum=warn"),
         ))
