a17704d678fa71403a73aa802926e9765d8a05af
Thomas Jensen <thomas.jensen_@outlook.com>
2026-04-29 17:06:57 +0200

    Update RPC client so it doesn't crash prerender build step

--- frontend/src/lib/rpc.ts
+++ frontend/src/lib/rpc.ts
@@ -1,5 +1,6 @@
 import { build_client, http } from '@qubit-rs/client';
 import type { QubitServer } from '../bindings';
 
-const api = build_client<QubitServer>(http(`${window.location.origin}/rpc`));
+const origin = typeof window !== 'undefined' ? window.location.origin : '';
+const api = build_client<QubitServer>(http(`${origin}/rpc`));
 export default api;
