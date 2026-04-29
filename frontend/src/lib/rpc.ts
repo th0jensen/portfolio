5a72a947762c0a6d781812fa46d894277d639667
Thomas Jensen <thomas.jensen_@outlook.com>
2026-04-29 16:37:31 +0200

    Fix hardcoded localhost

--- frontend/src/lib/rpc.ts
+++ frontend/src/lib/rpc.ts
@@ -1,5 +1,5 @@
 import { build_client, http } from '@qubit-rs/client';
 import type { QubitServer } from '../bindings';
 
-const api = build_client<QubitServer>(http('http://localhost:8080/rpc'));
+const api = build_client<QubitServer>(http(`${window.location.origin}/rpc`));
 export default api;
